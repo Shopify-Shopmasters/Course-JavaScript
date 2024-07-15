b90873f0194401e4a2962ff816f09abcb929f43a
Shopify-Shopmasters <myshopmasters@gmail.com>
2024-07-15 18:02:59 +0300

    Added homework 'STR' - 15/07/24

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//1. Получить первую и последнюю буквы строки//
+let str = "hello";
+console.log(getFirstAndLastChars(str));
+
+
+//2. Сделать первую и последнюю буквы в верхнем регистре//
+let str = "hello";
+console.log(getFirstAndLastChars) 
