62c085eab6779e425ae9c36bdbd3a42df8b226ca
Shopify-Shopmasters <myshopmasters@gmail.com>
2024-07-16 20:36:31 +0300

    Home work Numbers

--- js/index.js
+++ js/index.js
@@ -39,3 +39,67 @@ let a = 20;
 let b = 16;
 let string = a.toString() + b.toString();
 console.log(string);
+
+
+
+//№1 Математические операторы
+//Выведи на экран общее количество яблок и винограда. Разницу яблок и винограда.
+const apples = 47;
+const grapes = 135;
+const total = apples + grapes;
+console.log(total);
+const diff = apples - grapes;
+console.log(diff);
+
+//№2 Приоритет операторов
+//Разбери приоритет операторов в инструкции присвоения значения переменной result.
+const result = 108 + 223 - 2 * 5;
+console.log(result);
+
+//№3 Класс Math
+//Напиши скрипт, который выводит в консоль округленные вверх/вниз и т.д. значения переменной value. Используй методы Math.floor(), Math.ceil() и Math.round(). Проверь что будет в консоли при значениях 27.3 и 27.9.
+const value = 27.5;
+const valueHigh = Math.round(value);
+console.log(valueHigh);
+
+const valueDown = Math.floor(value);
+console.log(valueDown);
+
+const valueCeil = Math.ceil(value);
+console.log(valueCeil);
+
+//№4 Шаблонные строки
+//Составь фразу с помощью шаблонных строк A has B bots in stock, где A, B - переменные вставленные в строку.
+const companyName = 'Cyberdyne Systems';
+const repairBots = 150;
+const defenceBots = 50;
+const botsSum = repairBots + defenceBots;
+const message = `${companyName} has ${botsSum} bots in stock`;
+console.log(message); // "Cyberdyne Systems has 200 bots in stock"
+
+//№5 Методы строк и чейнинг
+let weight = '88.3';
+let height = '1.75';
+
+weight = Number(weight);
+height = Number(height);
+
+const bmi = weight / (height * height);
+const fixedBmi = bmi.toFixed(1);
+console.log(fixedBmi);
+
+//№6
+let nomNegative = 1;
+let nomCheck = isNaN(nomNegative);
+console.log(nomCheck);
+
+let firstWord = "Wow";
+let secondWord = "Interesting";
+let firstWordLetter = firstWord[0];
+let secondWordLetter = secondWord[0];
+let chooseWord = firstWordLetter == secondWordLetter;
+console.log(chooseWord);
+
+
+
+
